4744dfb5dc30a0787d609d9c208783f25b1d31ae
KhamzatKot <1000000000020z@gmail.com>
2023-03-04 01:31:24 +0300

    Это были тяжолы дни. Добавил страницу статей

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,10 +1,15 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../axios";
 
-export const fatchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-const { data} = await axios.get('/post');
+export const fatchPosts = createAsyncThunk('posts/fatchPosts', async () => {
+const { data} = await axios.get('/posts');
 return data;
 })
+
+export const fatchTags = createAsyncThunk('posts/fatchTags', async () => {
+    const { data} = await axios.get('/tags');
+    return data;
+    })
 
 const initialState = {
     posts: {
@@ -23,6 +28,7 @@ const postsSlice = createSlice({
     reducer: {},
     extraReducers: {
         [fatchPosts.pending]: (state) => {
+            state.posts.items = [];
             state.posts.state = 'loading'
         },
         [fatchPosts.fulfilled]: (state, action) => {
@@ -32,6 +38,18 @@ const postsSlice = createSlice({
         [fatchPosts.rejected]: (state) => {
             state.posts.items = [];
             state.posts.status = 'error';
+        },
+        [fatchTags.pending]: (state) => {
+            state.tags.items = [];
+            state.tags.state = 'loading'
+        },
+        [fatchTags.fulfilled]: (state, action) => {
+            state.tags.items = action.payload;
+            state.tags.status = 'loaded';
+        },
+        [fatchTags.rejected]: (state) => {
+            state.tags.items = [];
+            state.tags.status = 'error';
         },
     }
 })
